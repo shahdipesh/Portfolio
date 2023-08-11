e03e539951f27aecbabf0b380eafe30f94ad3a15
dipeshasd <dipesh.sah@hotmail.com>
2023-08-11 18:32:11 -0500

    advait chutiya update

--- src/App.js
+++ src/App.js
@@ -2,26 +2,14 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import './App.css';
-import NavBar from './Components/NavBar/NavBar';
-import Intro from './Components/Introduction/Intro';
-import Skills from './Components/Skills/Skills';
-import 'react-circular-progressbar/dist/styles.css';
-import ContactForm from './Components/Contact/ContactForm';
-import Projects from './Components/Projects/Projects';
-import Certificates from './Components/Certificates/Certificates';
+import Container from './Components/Container/Container';
 
 
 
 function App() {
   return (
     <div className="App">
-
-     <NavBar />
-     <Intro />
-     <Skills />
-     <Projects />
-     <Certificates />
-     <ContactForm />
+     <Container />
     </div>
   );
 }
