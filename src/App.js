6ba03f63cd6915cb123c70c171d6d3dc24d4fdf5
dipeshasd <dipesh.sah@hotmail.com>
2023-09-03 12:55:55 -0500

    remove advait update

--- src/App.js
+++ src/App.js
@@ -2,14 +2,26 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import './App.css';
-import Container from './Components/Container/Container';
+import NavBar from './Components/NavBar/NavBar';
+import Intro from './Components/Introduction/Intro';
+import Skills from './Components/Skills/Skills';
+import 'react-circular-progressbar/dist/styles.css';
+import ContactForm from './Components/Contact/ContactForm';
+import Projects from './Components/Projects/Projects';
+import Certificates from './Components/Certificates/Certificates';
 
 
 
 function App() {
   return (
     <div className="App">
-     <Container />
+
+     <NavBar />
+     <Intro />
+     <Skills />
+     <Projects />
+     <Certificates />
+     <ContactForm />
     </div>
   );
 }
